0c2ab241f593e373015c25135fc248777dd9706a
Daniel <aabccdaniel@gmail.com>
2023-06-10 16:40:58 +0800

    up data

--- dbJsonRead.js
+++ dbJsonRead.js
@@ -7,12 +7,38 @@ $.getJSON(url)
     db.level=msg["level"];
     db.range=msg.range;
     db.taskTime=msg.taskTime;
-    db.lastTaskTime=msg.lastTaskTime;
+    db.lastTaskDay=msg.lastTaskDay;
 })
 .fail(function(msg){
     console.log("Fail!");
 })
 let today=new Date().getDay;
-if(db.lastTaskTime!=today){
+if(db.lastTaskDay!=today){
     db.taskTime=0;
+}
+
+
+/*function dbUpdata(day,taskTime){
+    $.ajax({
+
+    })
+
+}*/
+
+
+
+function dbUpdata(day,taskTime){
+    let xmlHttpRequest;
+    if(window.XMLHttpRequest){
+        xmlHttpRequest=new XMLHttpRequest();
+    }else{
+        alert("No XMLHttpRequest!");
+        return;
+    }
+
+    xmlHttpRequest.open("PUT",url,true);
+    xmlHttpRequest.setRequestHeader("Content-type",'application/json');
+    xmlHttpRequest.send(`lastTaskDay=${day}`);
+    xmlHttpRequest.send(`taskTime=${taskTime}`);
+
 }
