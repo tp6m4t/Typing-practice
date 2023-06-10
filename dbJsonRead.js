6375928cf39d90f6a2cb1579709deb4fed45db0e
Daniel <aabccdaniel@gmail.com>
2023-06-10 15:27:40 +0800

    up data

--- dbJsonRead.js
+++ dbJsonRead.js
@@ -2,8 +2,12 @@ let url = "https://chkeybrjsonserverfinalproject.azurewebsites.net/keybr";
 let db;
 $.getJSON(url)
 .dene(function(msg){
+    console.log("資料庫連結成功");
     db.level=msg.level;
     db.range=msg.range;
     db.taskTime=msg.taskTime;
     db.lastTaskTime=msg.lastTaskTime;
+})
+.fail(function(msg){
+    console.log("Fail!");
 })
